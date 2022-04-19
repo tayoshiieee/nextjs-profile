73a938e28f726e7f7078555c9287e0ddbfbb03d4
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-04-19 22:01:16 +0900

    add skills section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        about: "url('/images/bg-about.png')",
+        future: "url('/images/bg-future.png')",
+        futureSp: "url('/images/bg-future-sp.png')",
+        skills: "url('/images/bg-skills.png')",
+        skillsSp: "url('/images/bg-skills-sp.png')",
+      }),
+    },
+    letterSpacing: {
+      wider: ".05em",
+      widest: ".15em",
+    },
   },
   plugins: [],
+  variants: {
+    borderWidth: ["responsive", "first", "hover", "focus"],
+  },
 };
