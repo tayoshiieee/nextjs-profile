448f9c137fe9c8ba8a68e1cc6969d37c8b36f71c
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-04-30 20:18:37 +0900

    add post.js & .md

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,20 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { Link as ScrollLink } from "react-scroll";
+import { getPostsData } from "../lib/post";
 
-export default function Home() {
+//SSGの場合
+export const getStaticProps = async () => {
+  const allPostsData = getPostsData();
+  // console.log(allPostsData);
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+export default function Home({ allPostsData }) {
   return (
     <>
       <Head>
@@ -131,57 +143,23 @@ export default function Home() {
             </p>
           </div>
           <div className="mx-auto max-w-screen-lg md:flex justify-center">
-            <div className="mx-auto w-[320px] p-5">
-              <div className="text-center">
-                <Image
-                  src="/images/about-1.png"
-                  width={184}
-                  height={157}
-                  // layout="fill"
-                  objectFit="contain"
-                />
+            {allPostsData.map(({ index, image, subheading, text }) => (
+              <div key={index} className="mx-auto w-[320px] p-5">
+                <div className="text-center">
+                  <Image
+                    src={image}
+                    width={184}
+                    height={157}
+                    // layout="fill"
+                    objectFit="contain"
+                  />
+                </div>
+                <p className="text-[#008c8d] font-bold text-xl md:text-2xl text-center my-1 md:my-3">
+                  {subheading}
+                </p>
+                <p className="leading-7">{text}</p>
               </div>
-              <p className="text-[#008c8d] font-bold text-xl md:text-2xl text-center my-1 md:my-3">
-                趣味
-              </p>
-              <p className="leading-7">
-                つれづれなるまゝに日暮らし硯にむかひて心にうつりゆくよしなし事をそこはかとなく書きつくれば、あやしうこそものぐるほし。
-              </p>
-            </div>
-            <div className="mx-auto w-[320px] p-5">
-              <div className="text-center">
-                <Image
-                  src="/images/about-2.png"
-                  width={184}
-                  height={157}
-                  // layout="fill"
-                  objectFit="contain"
-                />
-              </div>
-              <p className="text-[#008c8d] font-bold text-xl md:text-2xl text-center my-1 md:my-3">
-                好きな食べ物
-              </p>
-              <p className="leading-7">
-                つれづれなるまゝに日暮らし硯にむかひて心にうつりゆくよしなし事をそこはかとなく書きつくれば、あやしうこそものぐるほしけれ。つれづれなるまゝに日暮らし硯にむかひて
-              </p>
-            </div>
-            <div className="mx-auto w-[320px] p-5">
-              <div className="text-center">
-                <Image
-                  src="/images/about-3.png"
-                  width={184}
-                  height={157}
-                  // layout="fill"
-                  objectFit="contain"
-                />
-              </div>
-              <p className="text-[#008c8d] font-bold text-xl md:text-2xl text-center my-1 md:my-3">
-                性格
-              </p>
-              <p className="leading-7">
-                つれづれなるまゝに日暮らし硯にむかひて心にうつりゆくよしなし事をそこはかとなく書きつくれば、
-              </p>
-            </div>
+            ))}
           </div>
         </section>
         <section
