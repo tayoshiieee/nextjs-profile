e1b1071bccf0aa29ae80b38006b3bf52c7e44826
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-05-02 12:15:15 +0900

    fix text contents

--- components/CardAbout.js
+++ components/CardAbout.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function CardAbout(props) {
   const { image, subheading, text } = props;
   return (
-    <div className="mx-auto w-[320px] p-5">
+    <div className="mx-auto max-w-sm w-[320px] p-5">
       <div className="text-center">
         <Image
           src={image}
@@ -14,7 +14,7 @@ function CardAbout(props) {
           objectFit="contain"
         />
       </div>
-      <p className="text-[#008c8d] font-bold text-xl md:text-2xl text-center my-1 md:my-3">
+      <p className="text-[#008c8d] font-bold text-xl md:text-2xl text-center my-1 md:my-4">
         {subheading}
       </p>
       <p className="leading-7">{text}</p>
